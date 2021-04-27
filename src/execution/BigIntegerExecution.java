65d4a4566204ac11a0254436915157b8ea0f4c9b
github <pragya.sharma2_cs19@gla.ac.in>
2021-04-27 21:14:27 +0530

    choice has been asked.

--- src/execution/BigIntegerExecution.java
+++ src/execution/BigIntegerExecution.java
@@ -1,7 +1,28 @@
 package execution;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class BigIntegerExecution {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        JOptionPane.showMessageDialog(
+                null,
+                "Choose any operation according to the need -: ADD, SUBTRACT, MULTIPLY, DIVIDE, EXIT ",
+                "SELECTION",
+                JOptionPane.PLAIN_MESSAGE
+        );
+        String inputChoice = JOptionPane.showInputDialog(
+                null,
+                "Enter Choice",
+                "Choice",
+                JOptionPane.PLAIN_MESSAGE
+        );
+        Choice choice = Choice.valueOf(inputChoice);
 
     }
+}
+
+enum Choice{
+    ADD, SUBTRACT, MULTIPLY, DIVIDE, EXIT
 }
